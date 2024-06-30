661ffed5812159743eb92eaf00cc65e4f5df82cf
mushabtinumbang <mushabtinumbang@gmail.com>
2024-06-30 07:03:02 +0700

    initialize bash commands template

---
+++ bash/run_predict_sentiments_example.sh
@@ -0,0 +1,8 @@
+export DATE='01-01-2024|30-06-2024' &&
+export DAILYFX='y' &&
+export ECONTIMES='y' &&
+export SUFFIX='test' &&
+export RUN_SCRAPER='y' &&
+export RUN_PREDICTION='y' &&
+export PREPARE_STREAMLIT='y' &&
+make predict-sentiments
