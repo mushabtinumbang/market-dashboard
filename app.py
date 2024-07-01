56298e4b151d4e585259fe5793163a1f8dc8f779
mushabtinumbang <mushabtinumbang@gmail.com>
2024-07-02 02:35:10 +0700

    feature: streamlit sentiment dashboard

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import streamlit.components.v1 as components
+
 from PIL import Image
 from src.utilities.streamlit import *
 from streamlit_option_menu import option_menu
 from datetime import datetime, timedelta
 
+from src.utilities.config_ import finbert_model_path
 
 # Set Layout Config
 st.set_page_config(page_title="Market Dashboard", page_icon=":chart_with_upwards_trend:",layout="wide")
@@ -33,10 +35,21 @@ if True:
 
     if "df" not in st.session_state:
         df = getdata()
+        df['date'] = pd.to_datetime(df['date'])
         st.session_state.df = df
 
     else:
          df = st.session_state.df
+
+    if "finbert_model" not in st.session_state:
+        finbert_model = get_finbert_model()
+        finbert_tokenizer = get_finbert_tokenizer()
+        st.session_state.finbert_model = finbert_model
+        st.session_state.finbert_tokenizer = finbert_tokenizer
+
+    else:
+         finbert_model = st.session_state.finbert_model
+         finbert_tokenizer = st.session_state.finbert_tokenizer
 
     # Make a Centered Text
     col1, col2, col3 = st.columns((14, 6, 6))
@@ -56,7 +69,7 @@ if True:
     # Generate Tabs
     listTabs = [
     "Dashboard",
-    "Scrape & Train News"
+    "Scrape & Predict News"
     ]
 
     # Show tabs
@@ -66,8 +79,150 @@ if True:
     
     # Dashboard
     if selected == "Dashboard":
-        spacecol, forexcol, volcol, datecol = st.columns((50, 6, 6, 12))
-        marketcol0, marketcol1 = st.columns((2, 3))
-        
-        with marketcol0:
+        # create cols
+        title1col, datecol = st.columns((9, 2))
+
+        # Overall Sentiment Part
+        with title1col:
+            stspace(2)
             st.write("### Overall Sentiment")
+
+        with datecol:
+            # Find the minimum date
+            min_date = df['date'].min().to_pydatetime()
+
+            # Find the maximum date
+            max_date = df['date'].max().to_pydatetime()
+            
+            # Date Input Widget
+            date_chosen = st.date_input("Select Date", value=(max_date, max_date), min_value=min_date, max_value=max_date, format="DD.MM.YYYY", disabled=False, label_visibility="visible")
+
+            # Proccess date
+            start_date = date_chosen[0].strftime('%Y-%m-%d')
+
+            # Get end date
+            try:
+                end_date = date_chosen[1].strftime('%Y-%m-%d')
+
+            # Hadnle errors
+            except Exception as e:
+                end_date = start_date
+            
+            # Compute Total Negatives, Neutrals, Positives, and NSS Score
+            filtered_df = filter_df_by_date(df, start_date=start_date, end_date=end_date)
+            neg, neut, pos, score = calculate_sentiment_metrics(filtered_df)
+
+            # Get Total News by source
+            total_dailyfx, total_econtimes, total_financialtimes = get_total_unique_sources(filtered_df)
+        
+        # Sample value
+        sentiment_score = round(score, 3)
+        sentiment_label = "Negative" if sentiment_score <= -0.2 else "Neutral" if sentiment_score < 0.2 else "Positive"
+        
+        # create dashboard cols
+        leftcol, midcol = st.columns(2)
+
+        with leftcol:
+            # Show Gauge Chart
+            st.plotly_chart(create_gauge_chart(sentiment_score))
+            
+        with midcol:
+            stspace(2)
+            st.write(f"**Overall Net Sentiment Score (NSS) for a given period is** ***{sentiment_label}***.")
+            mid_1, mid_2, mid_3 = st.columns((100, 100, 100))
+            
+            with mid_1:
+                # Give space :)
+                stspace(2)
+
+                # Negative Metrics
+                st.metric("Negative", f"{neg} News", "-2")
+
+            with mid_2:
+                # Give spaazeeee
+                stspace(2)
+
+                # Neutral Metrics
+                st.metric("Neutral", f"{neut} News", "4")
+            
+            with mid_3:
+                # Give space
+                stspace(2)
+
+                # Positive Metrics
+                st.metric("Positive", f"{pos} News", "3")
+
+            # Document news sources
+            stspace(3)
+            st.write(f"###### Total News Per Source ")
+            st.write(f"DailyFX : {total_dailyfx}. Economic Times : {total_econtimes}. Financial Times : {total_financialtimes}")
+            
+        # 2nd Header
+        title2col, _ = st.columns((9, 2))
+
+        with title2col:
+            # Write Header
+            st.write("### Predict News Headline Sentiment")
+        
+        # Create input widget
+        finbert_input = st.text_input("Input News Headline to Predict Sentiment", "")
+
+        if finbert_input:
+            finbert_output = predict_with_finbert(finbert_input, finbert_model, finbert_tokenizer)
+            st.write(f"###### Predicted Sentiment: {finbert_output[0]}")
+        
+        # 3rd Header
+        newscol1, newscol2 = st.columns((2))
+
+        with newscol1:
+            # Write Header
+            stspace(2)
+            st.write("### News")
+
+            # Loop through filtered_df
+            for index, row in filtered_df[:10].iterrows():
+                # Get Variables
+                title = row['title']
+                date = row['date']
+                url = row['url']
+                category = row['category']
+                label = row['label']
+                source = row['source']
+
+                color = {
+                    "negative": "red",
+                    "positive": "limegreen",
+                    "neutral": "blue"
+                }.get(label.lower(), "black")
+
+                st.markdown(f'<span style="color:{color};">{label.capitalize()}</span>', unsafe_allow_html=True)
+                st.markdown(f"##### <a href='{url}' style='color:black; font-size:20px;'>{title}</a>", unsafe_allow_html=True)
+                st.write(f'###### Source: {source.capitalize()}. Category: {category.capitalize()}. Date: {date.strftime("%d-%m-%Y")}')
+                
+                stspace(2)
+
+        
+        with newscol2:
+            stspace(5)
+            # Loop through filtered_df
+            for index, row in filtered_df[10:20].iterrows():
+                # Get Variables
+                title = row['title']
+                date = row['date']
+                url = row['url']
+                category = row['category']
+                label = row['label']
+                source = row['source']
+
+                color = {
+                    "negative": "red",
+                    "positive": "limegreen",
+                    "neutral": "blue"
+                }.get(label.lower(), "black")
+
+                st.markdown(f'<span style="color:{color};">{label.capitalize()}</span>', unsafe_allow_html=True)
+                st.markdown(f"##### <a href='{url}' style='color:black; font-size:20px;'>{title}</a>", unsafe_allow_html=True)
+                st.write(f'###### Source: {source.capitalize()}. Category: {category.capitalize()}. Date: {date.strftime("%d-%m-%Y")}')
+                
+                stspace(2)
+                    
--- src/utilities/streamlit.py
+++ src/utilities/streamlit.py
@@ -3,9 +3,20 @@ import time
 import pandas as pd
 import numpy as np
 import os
-from datetime import datetime
+import plotly.graph_objects as go
+import torch
+
 import src.utilities.utils as utils
+
+from datetime import datetime
 from src.utilities.config_ import combined_data_path
+from transformers import BertTokenizer, BertForSequenceClassification
+from src.utilities.config_ import finbert_model_path
+
+# Chart Variables
+quadrant_colors = ["#2bad4e", "#eff229", "#f25829"]  
+quadrant_text = ["<b>Negative</b>", "<b>Neutral</b>", "<b>Positive</b>"]
+n_quadrants = len(quadrant_colors) 
 
 def greet():
     st.toast('Hello!', icon='✅')
@@ -20,24 +31,142 @@ def getdata():
     df = utils.load(os.path.join(combined_data_path, 'combined_data.feather'))
     return df
 
-def ChangeButtonColour(widget_label, font_color, background_color='transparent'):
-    htmlstr = f"""
-        <script>
-            var elements = window.parent.document.querySelectorAll('button');
-            for (var i = 0; i < elements.length; ++i) {{ 
-                if (elements[i].innerText == '{widget_label}') {{ 
-                    elements[i].style.color ='{font_color}';
-                    elements[i].style.background = '{background_color}'
-                }}
-            }}
-        </script>
-        """
-    return (f"{htmlstr}")
+def get_finbert_model():
+    finbert_model = BertForSequenceClassification.from_pretrained(finbert_model_path)
+    return finbert_model
 
-def sentiment_category(sentimentlatest):
-    if sentimentlatest > 0.1:
-        return 'Positive'
-    elif sentimentlatest < -0.1:
-        return 'Negative'
-    else:
-        return 'Neutral'
+def get_finbert_tokenizer():
+    finbert_tokenizer = BertTokenizer.from_pretrained(finbert_model_path)
+    return finbert_tokenizer
+
+def create_gauge_chart(current_value):
+    # Configuration for the gauge chart
+    plot_bgcolor = "#fff"  # Default white background
+    quadrant_colors = [plot_bgcolor, "#2bad4e", "#eff229" , "#f25829"]  # Negative, Neutral, Positive
+    quadrant_text = ["", "<b>Positive</b>", "<b>Neutral</b>", "<b>Negative</b>"]
+    n_quadrants = len(quadrant_colors) - 1
+    
+    min_value = -1
+    max_value = 1
+    hand_length = np.sqrt(2) / 4
+    hand_angle = np.pi * (1 - (max(min_value, min(max_value, current_value)) - min_value) / (max_value - min_value))
+
+    fig = go.Figure(
+        data=[
+            go.Pie(
+                values=[0.5] + [0.2, 0.1, 0.2],
+                rotation=90,
+                hole=0.5,
+                marker_colors=quadrant_colors,
+                text=quadrant_text,
+                textinfo="text",
+                hoverinfo="skip",
+                sort=False
+            ),
+        ],
+        layout=go.Layout(
+            showlegend=False,
+            margin=dict(b=0, t=40, l=10, r=10),
+            width=400,
+            height=400  ,
+            paper_bgcolor=plot_bgcolor,
+            annotations=[
+                go.layout.Annotation(
+                    text=f"<b>Net Sentiment Score Value:</b><br>{current_value}",
+                    x=0.5, xanchor="center", xref="paper",
+                    y=0.25, yanchor="bottom", yref="paper",
+                    showarrow=False,
+                )
+            ],
+            shapes=[
+                go.layout.Shape(
+                    type="circle",
+                    x0=0.48, x1=0.52,
+                    y0=0.48, y1=0.52,
+                    fillcolor="#333",
+                    line_color="#333",
+                ),
+                go.layout.Shape(
+                    type="line",
+                    x0=0.5, x1=0.5 + hand_length * np.cos(hand_angle),
+                    y0=0.5, y1=0.5 + hand_length * np.sin(hand_angle),
+                    line=dict(color="#333", width=4)
+                )
+            ]
+        )
+    )
+    
+    return fig
+
+def filter_df_by_date(df, start_date, end_date):
+    # Filter the DataFrame based on the date range
+    filtered_df = df[(df['date'] >= start_date) & (df['date'] <= end_date)]
+    filtered_df = filtered_df.sort_values('date', ascending=False).reset_index(drop=True)
+    
+    return filtered_df
+
+def calculate_sentiment_metrics(df):
+    # count label
+    label_counts = df['label'].value_counts()
+
+    # get total for each label 
+    total_neutral = label_counts.get('neutral', 0)
+    total_positive = label_counts.get('positive', 0)
+    total_negative = label_counts.get('negative', 0)
+    
+    # Weighted Sentiment Score (weights can be adjusted as needed)
+    w_p = 1
+    w_n = 1
+    weighted_sentiment_score = (((w_p * total_positive) - (w_n * total_negative)) / (total_positive + total_negative + total_neutral))
+    
+    return total_negative, total_neutral, total_positive, weighted_sentiment_score
+
+def get_total_unique_sources(df):
+
+    # count label
+    label_counts = df['source'].value_counts()
+
+    # get total for each label 
+    total_dailyfx = label_counts.get('dailyfx', 0)
+    total_econtimes = label_counts.get('econtimes', 0)
+    total_financialtimes = label_counts.get('financialtimes', 0)
+    
+    return total_dailyfx, total_econtimes, total_financialtimes
+
+def predict_with_finbert(
+        text,
+        loaded_model,
+        loaded_tokenizer,
+        batch_size=32
+):
+    # New text data to predict
+    titles = [text]
+
+    # Move model to the appropriate device (e.g., CPU or GPU)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    loaded_model.to(device)
+
+    # Initialize an empty list to store predictions
+    all_predictions = []
+
+    # Process data in batches
+    for i in range(0, len(titles), batch_size):
+        batch_titles = titles[i:i + batch_size]
+        
+        # Tokenize the new text
+        inputs = loaded_tokenizer(batch_titles, padding='max_length', truncation=True, max_length=64, return_tensors="pt")
+        inputs = {key: value.to(device) for key, value in inputs.items()}
+        
+        # Make predictions
+        loaded_model.eval()
+        with torch.no_grad():
+            outputs = loaded_model(**inputs)
+            logits = outputs.logits
+            predictions = torch.argmax(logits, dim=-1)
+        
+        # Convert predictions to labels
+        int_to_label = {0: 'positive', 1: 'neutral', 2: 'negative'}
+        predicted_labels = [int_to_label[pred.item()] for pred in predictions]
+        all_predictions.extend(predicted_labels)
+
+    return all_predictions
