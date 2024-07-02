4f49debca16e173386ee73d2149faf5fa99db47a
mushabtinumbang <mushabtinumbang@gmail.com>
2024-07-02 18:11:34 +0700

    update bash commands tempalte

--- bash/run_latest_sentiment.sh
+++ bash/run_latest_sentiment.sh
@@ -1,8 +1,9 @@
 export DATE='latest' &&
 export DAILYFX='y' &&
 export ECONTIMES='y' &&
+export FINANCIALTIMES='y' &&
 export SUFFIX='test' &&
 export RUN_SCRAPER='y' &&
 export RUN_PREDICTION='y' &&
-export PREPARE_STREAMLIT='n' &&
+export PREPARE_STREAMLIT='y' &&
 make predict-sentiments
--- bash/run_predict_sentiments_example.sh
+++ bash/run_predict_sentiments_example.sh
@@ -1,9 +1,9 @@
-export DATE='01-06-2024|30-06-2024' &&
-export DAILYFX='y' &&
-export ECONTIMES='y' &&
+export DATE='29-06-2024|02-07-2024' &&
+export DAILYFX='n' &&
+export ECONTIMES='n' &&
 export FINANCIALTIMES='y' &&
 export SUFFIX='test' &&
 export RUN_SCRAPER='y' &&
-export RUN_PREDICTION='y' &&
-export PREPARE_STREAMLIT='y' &&
+export RUN_PREDICTION='n' &&
+export PREPARE_STREAMLIT='n' &&
 make predict-sentiments
